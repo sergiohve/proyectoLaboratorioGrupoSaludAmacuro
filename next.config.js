bfec0eaa812179b41cc7a0f450e59bebe83ccb1e
unknown <sherrera@ad3soft.com>
2025-12-09 16:34:12 -0400

    cambios

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { unoptimized: true } 
+  images: { unoptimized: true },
+  output: 'standalone'
 };
 
 module.exports = nextConfig;
